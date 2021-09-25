cd34ffb8ad293e37dbcfe627c72b7c6a2b89b481
Collin-W <whalencollin@gmail.com>
2021-09-24 21:20:00 -0500

    tasks change color based on time

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,38 @@
+var currentHour = moment().format('h');
+var hourColor = document.getElementById("div");
 
-var phreshSeconds = function ()  {
+
+var freshSeconds = function ()  {
     
     var dataTime = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("p#currentDay").html(dataTime);
 };
 
-phreshSeconds();
-setInterval(phreshSeconds, 1000);
+//function saves new task to  local storage
+
+//listen for event 
+//
 
 
-var currentHour = moment().format('h');
-console.log(currentHour);
+var colorTime = function() {
 
 
+    $("textarea").each(function(){
+        var textareaTime = parseInt($(this).attr("id"));
 
+        if (textareaTime <currentHour) {
+            $(this).addClass("past");
+        } else if  (currentHour === textareaTime) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("future");
+        }
+    })
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+freshSeconds();
+setInterval(freshSeconds, 1000);
+setInterval(colorTime, 20000);
 
 
 
@@ -45,6 +45,46 @@ console.log(currentHour);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var pastHour = (parseInt(currentHour) -1)
+
+// var futureHour = (parseInt(currentHour) +1)
+
+// var
+
+// //if future then all equal and greater are green
+// // if past the all equal and less are red
+
+
+// var aM = function() {
+//     if (currentHour <= 9 && currentHour >= 12) {
+
+//         pastHour
+//         futureHour = green
+//     }
+// }
+
+// var pM = function() {
+//     if (currentHour <= 1 && currentHour >= 5) {
+
+//         pastHour = red
+//         futureHour = green
+
+//     }
+// }
 
 // var welcomeDate = document.getElementById("currentDay");
 // var currentMoment = document.createTextNode("current moment in time");
